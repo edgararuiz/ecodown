e861d7d6cbe85a3b7f8783930e796b92835eab53
Edgar Ruiz <edgararuiz@gmail.com>
2021-12-02 13:41:58 -0600

    Improves file copy

--- R/package_files.R
+++ R/package_files.R
@@ -46,23 +46,30 @@ package_file_copy <- function(pkg_folder = "",
                               override_name = NULL,
                               project_folder = "",
                               root_folder = here::here()) {
+  
   file_present <- file_exists(path(pkg_folder, file_names))
   file_numbers <- setNames(file_present, 1:length(file_present))
   file_there <- file_numbers[file_numbers == TRUE]
-  file_min <- min(as.integer(names(file_there)))
-  file_use <- file_present[file_min]
-  file_name <- names(file_use)
+  
+  if(length(file_there) > 0) {
+    file_min <- min(as.integer(names(file_there)))
+    file_use <- file_present[file_min]
+    file_name <- names(file_use)
+    
+    dest_folder <- path(root_folder, project_folder, target)
+    
+    create_folder_if_missing(dest_folder)
+    
+    file_n <- ifelse(is.null(override_name), path_file(file_name), override_name)
+    
+    file_copy(
+      file_name,
+      path(dest_folder, file_n),
+      overwrite = TRUE
+    )
+    msg_green("Copied: ", path(project_folder, target, file_n))    
+  } else {
+    msg_yellow("File(s) not found: ", paste0(file_names, collapse = ", "))
+  }
 
-  dest_folder <- path(root_folder, project_folder, target)
-
-  create_folder_if_missing(dest_folder)
-
-  file_n <- ifelse(is.null(override_name), path_file(file_name), override_name)
-
-  file_copy(
-    file_name,
-    path(dest_folder, file_n),
-    overwrite = TRUE
-  )
-  msg_green("Copied: ", path(project_folder, target, file_n))
 }
