f1490e1c218f0b64febd4893e0baea36728cf510
Edgar Ruiz <edgararuiz@gmail.com>
2021-12-08 08:57:07 -0600

    Moving test package to testthat folder, emulating how pkgdown tests

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -1,18 +1,10 @@
 test_that("Full package documentation works", {
   
-  site_folder <- "tidypredict_site"
+  pkg_location <- test_path("assets/tidypredict")
+  
+  site_folder <- "test_site"
+  
   temp_dir <- tempdir()
-  pkg_folder <- "testutils/tidypredict"
-  
-  location_inst <- system.file(package = "ecodown", pkg_folder)
-  location_source <- paste0("inst/", pkg_folder)
-  
-  if(dir.exists(location_source)) {
-    pkg_location <- location_source
-  } else {
-    pkg_location <- location_inst
-  }
-  
   
   expect_output(
     package_build_documentation(
