d7bd3c1e3916d3da10a84a6ea24d0f0eca10ce5c
Edgar Ruiz <edgararuiz@gmail.com>
2021-12-29 11:29:16 -0600

    Switches test to use ecodown

--- tests/testthat/test-clone_convert.R
+++ tests/testthat/test-clone_convert.R
@@ -3,18 +3,18 @@ test_that("Default cloning works", {
  
   capture.output(
     ecodown_clone_convert(
-      repo_url = "https://github.com/tidyverse/purrr",
+      repo_url = "https://github.com/edgararuiz/ecodown",
       target_folder = paste0(tempdir(), "/clone1"),
       quarto_folder = paste0(tempdir(), "/site1"),
-      quarto_sub_folder = "purrr1"
+      quarto_sub_folder = "package1"
     )    
   )
   
-  top_files <- path_file(dir_ls(path(tempdir(), "site1", "purrr1"))) 
+  top_files <- path_file(dir_ls(path(tempdir(), "site1", "package1"))) 
   
-  expect_true("articles" %in% top_files)
+  #expect_true("articles" %in% top_files)
   expect_true("index.md" %in% top_files)
-  expect_true("news.md" %in% top_files)
+  #expect_true("news.md" %in% top_files)
   expect_true("reference" %in% top_files)
   
 })
