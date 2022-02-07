3747f8480812c11a28e0ccec893227906e29c23b
Edgar Ruiz <edgararuiz@gmail.com>
2022-02-07 15:24:58 -0600

    Render and autolink now runs per doc

--- R/quarto.R
+++ R/quarto.R
@@ -1,11 +1,13 @@
 #' Wrapper on the Quarto Render function
+#' @param autolink Boolean flag that indicates if 'downlit' should run. Defaults
+#' to TRUE.
 #' @inheritParams ecodown_clone_convert
 #' @export
 ecodown_quarto_render <- function(quarto_folder = here::here(),
-                                  verbosity = c("verbose", "summary", "silent")
+                                  verbosity = c("verbose", "summary", "silent"),
+                                  autolink = TRUE
                                   ) {
   verbosity <- verbosity[1]
-  
   ecodown_context_set("verbosity", verbosity)
 
   msg_summary_entry("\n")
@@ -16,32 +18,45 @@ ecodown_quarto_render <- function(quarto_folder = here::here(),
     dir_ls(quarto_folder, recurse = TRUE, glob = "*.Rmd"),
     dir_ls(quarto_folder, recurse = TRUE, glob = "*.qmd")
   )
+  
+  if(autolink) downlit_env(quarto_folder = quarto_folder)
 
   file_tree(
     file_list = rend_files,
     file_type = "renderable ",
     base_folder = quarto_folder,
     command_name = "render_quarto",
-    verbosity = verbosity
+    verbosity = verbosity, 
+    addl_entries = list(
+      autolink = autolink, 
+      quarto_folder = quarto_folder
+      )
   )
 }
 
-render_quarto <- function(input) {
+render_quarto <- function(input, autolink, quarto_folder) {
   if(!is.null(quarto_path())) {
     if(tolower(path_ext(input) == "qmd")) {
-      quarto_render(
-        input, 
-        as_job = FALSE, 
-        quiet = TRUE, 
-        pandoc_args = c("--use-freezer")
-      )      
+      p_args <- c("--use-freezer")
     } else {
-      quarto_render(
-        input, 
-        as_job = FALSE, 
-        quiet = TRUE
-      ) 
+      p_args <- NULL
     }
-
-  } 
+    quarto_render(
+      input = input, 
+      as_job = FALSE, 
+      quiet = TRUE, 
+      pandoc_args = p_args
+    ) 
+    if(autolink) {
+      co <- path_common(c(quarto_folder, input))
+      ne <- path_dir(substr(input, nchar(co) + 1, nchar(input)))
+      fi <- path(path_ext_remove(path_file(input)), ext = "html")
+      od <- qe(quarto_folder, "project", "output-dir")
+      input_html <- path(quarto_folder, od, ne, fi)
+      if(!file_exists(input_html)) {
+        input_html <- path(path_ext_remove(input), ext = "html")
+      } 
+      downlit_single(input = input_html)
+    }
+  }
 }
