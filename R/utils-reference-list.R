ec2c9fd5d36aee4d21ff2ca554a73d6a8dc081a2
Edgar Ruiz <edgararuiz@gmail.com>
2022-08-22 13:02:50 -0500

    Parses all sparklyr

--- R/utils-reference-list.R
+++ R/utils-reference-list.R
@@ -6,8 +6,10 @@ reference_to_list_page <- function(file_in, pkg) {
 
 tags_get <- function(file_in, pkg) {
   if(is.character(pkg)) pkg <- as_pkgdown(pkg)
+  
   pkg_topics <- pkg$topics
   topic_row <- pkg_topics[pkg_topics$file_in == file_in, ]
+
   topic <- transpose(topic_row)
   topic_rd <- topic[[1]]$rd
   tag_names <- map_chr(topic_rd, ~ class(.)[[1]])
@@ -66,7 +68,7 @@ tag_convert.tag_section <- function(x) {
   })
 }
 
-tag_convert.tag_examples <- function(x) {
+tag_examples <- function(x) {
   x <- x[[1]]
   rf <- tag_flatten(x)
   on_examples <- TRUE
@@ -97,8 +99,10 @@ tag_convert.tag_examples <- function(x) {
   )
 }
 
+tag_convert.tag_examples <- tag_examples
+
 tag_convert.tag_usage <- function(x) {
-  out <- tag_convert.tag_examples(x)
+  out <- tag_examples(x)
   out$code_run
 }
 
@@ -122,70 +126,59 @@ tag_convert.tag_arguments <- function(x) {
 
 ## -------------------------------- Tag translation ----------------------------
 
-tag_single_base <- function(x) {
-  res <- NULL
-  x_class <- class(x)[[1]]
-  if(x_class == "TEXT") res <- x
-  if(x_class == "RCODE") res <- x
-  if(x_class == "VERB") res <- x
-  if(x_class == "tag_method") res <- tag_method(x)
-  if(x_class == "tag_href") res <- tag_href(x)
-  if(x_class == "tag_code") res <- tag_code(x)
-  if(x_class == "tag_verb") res <- tag_code(x)
-  if(x_class == "tag_pkg") res <- tag_code(x)
-  if(x_class == "tag_usage") res <- tag_code(x)
-  if(x_class == "tag_url") res <- tag_url(x)
-  if(x_class == "tag_if") res <- ""
-  if(x_class == "tag_cr") res <- ""
-  if(x_class == "tag_R") res <- "`R`"
-  if(x_class == "tag_link") res <- as.character(x[[1]])
-  if(x_class == "tag_preformatted") res <- tag_preformatted(x)
-  if(x_class == "tag_emph") res <- paste0("**", x, "**")
-  if(x_class == "tag_strong") res <- paste0("**", x, "**")
-  if(x_class == "tag_cite") res <- paste0("*", x, "*")
-  if(x_class == "tag_email") res <- tag_url(x)
-  if(x_class == "tag_itemize") res <- tag_itemize1(x)
-  if(class(x)[[1]] == "tag_tabular") res <- ""
-  res
+tag_translate <- function(x) {
+  UseMethod("tag_translate")
 }
 
-tag_single <- function(x) {
-  res <- tag_single_base(x)
-  x_class <- class(x)[[1]]
-  if(x_class == "tag_dontrun") res <- tag_dontrun(x)
-  if(x_class == "tag_enumerate") res <- tag_itemize1(x)
-  if(x_class == "tag_describe") res <- tag_describe(x)
-  if(x_class == "tag_subsection") res <- tag_sub_section(x)
-  if(x_class == "LIST") res <- tag_LIST(x)
-  if(is.null(res)) stop(paste0("Class '", class(x)[[1]], "' not recognized. Value: ", x))
-  res <- map_chr(res, remove_return)
+tag_translate.TEXT <- function(x) x
+tag_translate.RCODE <- function(x) x
+tag_translate.VERB <- function(x) x
+tag_translate.character <- function(x) x
+
+tag_translate.tag_code <- function(x) tag_code(x)
+tag_translate.tag_samp <- function(x) tag_code(x)
+tag_translate.tag_verb <- function(x) tag_code(x)
+tag_translate.tag_pkg <- function(x) tag_code(x)
+tag_translate.tag_usage <- function(x) tag_code(x)
+
+tag_translate.tag_if <- function(x) ""
+tag_translate.tag_cr <- function(x) ""
+tag_translate.tag_tabular <- function(x) ""
+
+
+tag_translate.tag_R <- function(x) "`R`"
+tag_translate.tag_link <- function(x) as.character(x[[1]])
+tag_translate.tag_emph <- function(x) paste0("**", x, "**")
+tag_translate.tag_strong <- function(x) paste0("**", x, "**")
+tag_translate.tag_cite <- function(x) paste0("*", x, "*")
+tag_translate.tag_eqn <- function(x) paste0("$", x, "$")
+tag_translate.tag_item <- function(x) list(new_paragraph_symbol, "-")
+
+tag_translate.tag_preformatted <- function(x) tag_preformatted(x)
+tag_translate.tag_email <- function(x) tag_url(x)
+tag_translate.tag_url <- function(x) tag_url(x)
+tag_translate.tag_itemize <- function(x) tag_itemize1(x)
+tag_translate.tag_dontrun <- function(x) tag_dontrun(x)
+tag_translate.tag_enumerate <- function(x) tag_itemize1(x)
+tag_translate.tag_describe <- function(x) tag_describe(x)
+tag_translate.tag_subsection <- function(x) tag_sub_section(x)
+tag_translate.tag_method <- function(x) tag_method(x)
+tag_translate.tag_href <- function(x) tag_href(x)
+tag_translate.LIST <- function(x) tag_LIST(x)
+
+tag_translate.default <- function(x) {
+  stop(paste0("Class '", class(x)[[1]], "' not recognized. Value: ", x))
 }
 
-tag_LIST <- function(x) {
-  x %>% 
-    map(tag_single) %>% 
-    paste(collapse = "") %>% 
-    paste0("\n")
-}
+## Revisit when able ---------
+tag_translate.USERMACRO <- function(x) x[[2]]
+tag_translate.tag_Sexpr <- function(x) ""
+## ----------------------------
 
-tag_describe <- function(x) {
-  out <- x %>%  
-    flatten() %>% 
-    map(~.x[[1]]) %>% 
-    map(tag_single_base)
-  out_nulls <- !map_lgl(out, is.null)
-  out <- out[out_nulls]
-  
-  out <- reduce(out, function(x, y) c(x, new_paragraph_symbol, y))
-  
+tag_single <- function(x, rm_return = TRUE) {
+  out <- tag_translate(x)
+  if(rm_return) out <- map_chr(out, remove_return)
   out
-}
-
-tag_single_item <- function(x) {
-  res <- tag_single_base(x)
-  if(class(x)[[1]] == "tag_item") res <- list(new_paragraph_symbol, "-")
-  if(is.null(res)) stop(paste0("Class '", class(x)[[1]], "' not recognized"))
-  res
 }
 
 tag_flatten <-  function(x) {
@@ -218,19 +211,36 @@ remove_generic <- function(x) {
 
 ## -------------------------- Atomic RD tag functions ---------------------------
 
-tag_dontrun <- function(x) {
-  out <- x %>% 
+tag_LIST <- function(x) {
+  x %>% 
     map(tag_single) %>% 
-    flatten()
+    paste(collapse = "") %>% 
+    paste0("\n")
+}
+
+tag_describe <- function(x) {
+  out <- x %>%  
+    flatten() %>% 
+    map(~.x[[1]]) %>% 
+    map(tag_single)
+  out_nulls <- !map_lgl(out, is.null)
+  out <- out[out_nulls]
   
-  c(do_not_run_symbol, out)
+  reduce(out, function(x, y) c(x, new_paragraph_symbol, y))
+}
+
+tag_dontrun <- function(x) {
+  x %>% 
+    map(tag_single) %>% 
+    flatten() %>% 
+    c(do_not_run_symbol, .)
 }
 
 
 tag_sub_section <- function(x) {
   x_class <- map_chr(x, class)
   if(any(x_class == "tag")) {
-    out <- map(x, map, tag_single)
+    out <- map(x, function(x) flatten(map(x, tag_single)))
   } else {
     out <-  map(x, tag_single)   
   }
@@ -243,7 +253,7 @@ tag_sub_section <- function(x) {
 
 tag_itemize1 <- function(x) {
   x %>% 
-    map(tag_single_item) %>% 
+    map(tag_single, FALSE) %>% 
     flatten() %>% 
     map(remove_return) %>% 
     c(., new_paragraph_symbol)
@@ -251,7 +261,7 @@ tag_itemize1 <- function(x) {
 
 tag_code <- function(x) {
   x %>% 
-    map(tag_single_base) %>% 
+    map(tag_single) %>% 
     reduce(paste0) %>% 
     paste0("`", ., "`")
 }
@@ -270,7 +280,7 @@ tag_url <- function(x) {
 
 tag_href <- function(x) {
   address <- as.character(x[[1]])
-  label_list <- map_chr(x[[2]], tag_single_base)
+  label_list <- map_chr(x[[2]], tag_single)
   label <- paste0(label_list, collapse = " ")
   res <- paste0("[", label, "](", address, ")")
 }
