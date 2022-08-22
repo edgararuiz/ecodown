845f193a0d0a8636f95f7e9c17e8cdccaa95c235
Edgar Ruiz <edgararuiz@gmail.com>
2022-08-22 13:35:14 -0500

    Adds env var support for example code run

--- R/examples.R
+++ R/examples.R
@@ -10,12 +10,18 @@ examples_not_run <- function() {
 
 examples_run_not_run <- function(x = "") {
   out <- FALSE
-  yml_path <- path("_ecodown.yml")
-  if(file_exists(yml_path)) {
-    yml_file <- read_yaml(yml_path)  
-    re <- yml_file$site$clone_convert
-    if (!is.null(re[[x]]))  out <- re[[x]]
+  env_var <- Sys.getenv(paste0("ecodown_", x), unset = NULL)
+  if(is.null(env_var)) {
+    yml_path <- path("_ecodown.yml")
+    if(file_exists(yml_path)) {
+      yml_file <- read_yaml(yml_path)  
+      re <- yml_file$site$clone_convert
+      if (!is.null(re[[x]]))  out <- re[[x]]
+    }  
+  } else {
+    out <- env_var
   }
+  
   out
 }
 
