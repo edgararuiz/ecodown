f6495514d2160b5fb9f1bf13a74585ea10e4d410
Edgar Ruiz <edgararuiz@gmail.com>
2022-08-24 11:20:47 -0500

    Switches environment check default from null to na

--- R/examples.R
+++ R/examples.R
@@ -15,8 +15,8 @@ examples_not_run <- function() {
 
 examples_run_not_run <- function(x = "") {
   out <- FALSE
-  env_var <- Sys.getenv(paste0("ecodown_", x), unset = NULL)
-  if(is.null(env_var)) {
+  env_var <- Sys.getenv(paste0("ecodown_", x), unset = NA)
+  if(is.na(env_var)) {
     yml_path <- path("_ecodown.yml")
     if(file_exists(yml_path)) {
       yml_file <- read_yaml(yml_path)  
